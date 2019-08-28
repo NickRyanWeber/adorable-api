0c494a8782db9409df7c9a274ac229a130ec314b
nickryanweber <nicholasryanweber@gmail.com>
2019-08-28 16:50:30 -0400

    got HSL to work

--- src/components/Adorable.jsx
+++ src/components/Adorable.jsx
@@ -3,20 +3,80 @@ import M from 'materialize-css'
 import axios from 'axios'
 
 const Adorable = () => {
-  const [mouth, setMouth] = useState(1)
-  const [eyes, setEyes] = useState(1)
-  const [nose, setNose] = useState(1)
-  let URL = `https://api.adorable.io/avatars/face/eyes${eyes}/nose${nose}/mouth${mouth}/1B5AE1`
+  const [mouth, setMouth] = useState(0)
+  const [eyes, setEyes] = useState(0)
+  const [nose, setNose] = useState(0)
+  const [url, setUrl] = useState()
+  const [eyesArr, setEyesArr] = useState([])
+  const [noseArr, setNoseArr] = useState([])
+  const [mouthArr, setMouthArr] = useState([])
+  const [hex, setHex] = useState('1B5AE1')
+  const [hue, setHue] = useState(218)
+  const [saturation, setSaturation] = useState(77)
+  const [lightness, setLightness] = useState(54)
 
-  let apiData = {}
+  useEffect(() => {
+    const url = `https://api.adorable.io/avatars/face/${eyesArr[eyes]}/${noseArr[nose]}/${mouthArr[mouth]}/${hex}`
+    setUrl(url)
+  }, [mouth, eyes, nose, hex])
 
   const fetchData = async () => {
     const resp = await axios.get(
       `https://api.allorigins.win/get?url=https://api.adorable.io/avatars/list`
     )
-    // console.log(JSON.parse(resp.data.contents))
-    apiData = JSON.parse(resp.data.contents)
-    console.log(apiData.face.eyes)
+    const apiData = JSON.parse(resp.data.contents)
+    setEyesArr(apiData.face.eyes)
+    setNoseArr(apiData.face.nose)
+    setMouthArr(apiData.face.mouth)
+  }
+
+  const HSLToHex = (h, s, l) => {
+    s /= 100
+    l /= 100
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+      m = l - c / 2,
+      r = 0,
+      g = 0,
+      b = 0
+
+    if (0 <= h && h < 60) {
+      r = c
+      g = x
+      b = 0
+    } else if (60 <= h && h < 120) {
+      r = x
+      g = c
+      b = 0
+    } else if (120 <= h && h < 180) {
+      r = 0
+      g = c
+      b = x
+    } else if (180 <= h && h < 240) {
+      r = 0
+      g = x
+      b = c
+    } else if (240 <= h && h < 300) {
+      r = x
+      g = 0
+      b = c
+    } else if (300 <= h && h < 360) {
+      r = c
+      g = 0
+      b = x
+    }
+    // Having obtained RGB, convert channels to hex
+    r = Math.round((r + m) * 255).toString(16)
+    g = Math.round((g + m) * 255).toString(16)
+    b = Math.round((b + m) * 255).toString(16)
+
+    // Prepend 0s, if necessary
+    if (r.length == 1) r = '0' + r
+    if (g.length == 1) g = '0' + g
+    if (b.length == 1) b = '0' + b
+
+    return r + g + b
   }
 
   useEffect(() => {
@@ -24,54 +84,88 @@ const Adorable = () => {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    console.log({ hue, saturation, lightness })
+    setHex(HSLToHex(hue, saturation, lightness))
+    console.log(hex)
+  }, [hue, saturation, lightness])
+
   return (
     <>
       <main className="container">
-        <h1 className="center-align">Adorable Avatar Selector</h1>
+        <h3 className="center-align">Adorable Avatar Selector</h3>
         <div className="row">
-          <div className="col s5 offset-s3">
+          <div className="col s6 offset-s3">
             <main className="card">
               <section className="card-image">
-                <img src={URL} alt="" />
+                <img src={url} alt="" />
               </section>
-              <section className="col s8 offset-s2">
+              <section className="col s6">
                 <br />
-                <p className="range-field center">
-                  Eyes
-                  <input
-                    type="range"
-                    min="1"
-                    max="9"
-                    value={eyes}
-                    onChange={e => {
-                      setEyes(e.target.value)
-                    }}
-                  />
-                </p>
-                <p className="range-field center">
-                  Nose
-                  <input
-                    type="range"
-                    min="1"
-                    max="8"
-                    value={nose}
-                    onChange={e => {
-                      setNose(e.target.value)
-                    }}
-                  />
-                </p>
-                <p className="range-field center">
-                  Mouth
-                  <input
-                    type="range"
-                    min="1"
-                    max="8"
-                    value={mouth}
-                    onChange={e => {
-                      setMouth(e.target.value)
-                    }}
-                  />
-                </p>
+                <label className="range-field center">Eyes</label>
+                <input
+                  type="range"
+                  min="0"
+                  max={eyesArr.length}
+                  value={eyes}
+                  onChange={e => {
+                    setEyes(e.target.value)
+                  }}
+                />
+                <label className="range-field center">Nose</label>
+                <input
+                  type="range"
+                  min="0"
+                  max={noseArr.length}
+                  value={nose}
+                  onChange={e => {
+                    setNose(e.target.value)
+                  }}
+                />
+                <label className="range-field center">Mouth</label>
+                <input
+                  type="range"
+                  min="0"
+                  max={mouthArr.length}
+                  value={mouth}
+                  onChange={e => {
+                    setMouth(e.target.value)
+                  }}
+                />
+                <br />
+              </section>
+              <section className="col s6">
+                <br />
+                <label className="range-field center">Hue</label>
+                <input
+                  type="range"
+                  min="0"
+                  max="360"
+                  value={hue}
+                  onChange={e => {
+                    setHue(e.target.value)
+                  }}
+                />
+                <label className="range-field center">Saturation</label>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={saturation}
+                  onChange={e => {
+                    setSaturation(e.target.value)
+                  }}
+                />
+                <label className="range-field center">Lightness</label>
+                <input
+                  type="range"
+                  min="0"
+                  max="100"
+                  value={lightness}
+                  onChange={e => {
+                    setLightness(e.target.value)
+                  }}
+                />
                 <br />
               </section>
             </main>
